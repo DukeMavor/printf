37f019eb660d4f9ba0ac7d24bd5812ea52f9d80d
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2022-11-18 06:44:12 +0100

    Create conv_s.c

---
+++ conv_s.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * conv_s - function to print a null-terminated string
+ * @spec: struct containing formatting information for `conv_s'
+ * @ap: variable argument list
+ *
+ * Return: Number of bytes printed
+ */
+int conv_s(spec_t *spec, va_list ap)
+{
+	int tbytes = 0;
+	int prec = spec->prec;
+	char *s = va_arg(ap, char *);
+
+	if (s == NULL)
+		s = "(null)";
+	if (prec >= 0)
+		while (*s && prec--)
+			writetobuf(s++, 1);
+	else
+		while (*s)
+			writetobuf(s++, 1);
+	return (tbytes);
+}
