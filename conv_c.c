754bd94f4030b7819bafa084f4634e279eccc4b9
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2022-11-18 06:42:54 +0100

    Create conv_c.c

---
+++ conv_c.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * conv_c - function to format printing a single character
+ * @spec: struct containing formatting parameters
+ * @ap: variable argument list
+ *
+ * Return: Number of bytes printed
+ */
+int conv_c(spec_t *spec, va_list ap)
+{
+	int i, tbytes = 0;
+	unsigned char c;
+
+	c = (unsigned char) va_arg(ap, int);
+
+	i = spec->length;
+	if (spec->flags & JUST_FLAG)
+	{
+		writetobuf(&c, 1);
+		while (--i >= 0)
+			writetobuf(" ", 1);
+	}
+	else
+	{
+		while (--i >= 0)
+			writetobuf(" ", 1);
+		writetobuf(&c, 1);
+	}
+	return (tbytes);
+}
