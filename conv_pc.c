3538be45294de5f8980db6b300a765b03b2c3b01
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2022-11-19 12:13:21 +0100

    Create conv_pc.c

---
+++ conv_pc.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * conv_pc - function to print a single `%' preceded by a `%'
+ * @spec: (unused) struct to format printing of inputs
+ * @ap: (unused) variable argument list
+ *
+ * Return: Number of bytes printed
+ */
+int conv_pc(spec_t *spec, va_list ap)
+{
+	(void)spec;
+	(void)ap;
+	writetobuf("%", 1);
+	return (1);
+}
