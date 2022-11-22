ba49e024ab48b8e8d1d4c81feb0e3dd2736e60e4
Murendwaratshitimba <murendwaratshitimba@gmail.com>
2022-11-22 23:18:49 +0200

    base funstions fixed

--- base_functions.c
+++ base_functions.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * print_unsigned - Prints an unsigned number
  *
@@ -22,6 +21,7 @@ int print_unsigned(va_list types, char buffer[],
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
+
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
