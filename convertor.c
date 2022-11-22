22d62c8a64c0a2971b2de219254dd1ad6ffd3eb1
Murendwaratshitimba <murendwaratshitimba@gmail.com>
2022-11-22 23:21:18 +0200

    betty convertor fix

--- convertor.c
+++ convertor.c
@@ -16,11 +16,11 @@ int is_printable(char c)
 
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
- * 
+ *
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
- * 
+ *
  * Return: Always 3 (int)
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
